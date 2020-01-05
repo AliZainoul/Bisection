5767ff2cd2b5657da99b515935d86c5aed9aa512
AliZainoul <Ali.Zainoul.az@gmail.com>
2020-01-05 02:32:47 +0100

    Update

--- bisection.cpp
+++ bisection.cpp
@@ -8,8 +8,7 @@ typedef double (*pfn) (double);
 double bisection(double a, double b, pfn f , double delta, double epsilon)
 {
   double midpoint = (a+b)/ 2;
-  while (((abs(b-a)) > delta) && ((abs(f(midpoint))) > epsilon)
-)
+  if (((abs(b-a)) > delta) || ((abs(f(midpoint))) > epsilon))
   {
     if (f(a)*f(midpoint) < 0) return bisection (a, b=midpoint, f, delta, epsilon);
     else return bisection(a=midpoint, b, f, delta, epsilon);
--- main_bisection.cpp
+++ main_bisection.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-//#include "Vector.hpp"
 #include "error.hpp"
 #include "bisection.hpp"
 
@@ -10,6 +8,7 @@ typedef double (*pfn) (double);
 
 double f(double x){return (1/x) - pow(2,x);};
 double g(double x){return pow(2,(-x)) + exp(x) + 2 * cos(x) - 6;};
+
 double p(double x){return x+1;}
 double q(double x){return (x*x)-1;};
 double r(double x){return (x*x*x)-(5*x*x)+(3*x)+7;};
@@ -18,42 +17,38 @@ double s(double x){return 1-(x*x);};
 
 int main ()
 {
-cout << "*** **** **** ****"<< "\n";
-cout << "Function f evaluated at x=1 : " << f(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(0, 1, f, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
-
+  double delta, epsilon;
+  delta = 0.00001;
+  epsilon = 0.00000001 ;
 
 cout << "*** **** **** ****"<< "\n";
-cout << "Function g evaluated at x=1 :  " << g(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(1, 3, g, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(0, 1, f, delta, epsilon)<< " is "
+<< f(bisection(0, 1, f, delta, epsilon))<<"\n";
+getchar();
 
 cout << "*** **** **** ****"<< "\n";
-cout << "Function p evaluated at x=1 :  " << p(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(-2, 1, p, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(1, 3, g, delta, epsilon)<< " is "
+<< g(bisection(1, 3, g, delta, epsilon))<<"\n";
+getchar();
 
 cout << "*** **** **** ****"<< "\n";
-cout << "Function q evaluated at x=1 : " << q(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(-1, 1, q, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(-2, 1, p, delta, epsilon)<< " is "
+<< p(bisection(-2, 1, p, delta, epsilon))<<"\n";
+getchar();
 
 cout << "*** **** **** ****"<< "\n";
-cout << "Function r evaluated at x=1 : " << r(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(0, 3, r, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(-1, 1, q, delta, epsilon)<< " is "
+<< q(bisection(-1, 1, q, delta, epsilon))<<"\n";
+getchar();
 
 cout << "*** **** **** ****"<< "\n";
-cout << "Function s evaluated at x=1 : " << s(1) << "\n";
-cout << "*** **** **** ****"<< "\n";
-cout << bisection(-3, 3, s, 0.000000001, 0.000000001)<< "\n";
-cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(0, 3, r, delta, epsilon)<< " is "
+<< r(bisection(0, 3, r, delta, epsilon))<<"\n";
+getchar();
 
+cout << "*** **** **** ****"<< "\n";
+cout << " Function evaluated at " <<bisection(-3, 3, s, delta, epsilon)<< " is "
+<< s(bisection(-3, 3, s, delta, epsilon))<<"\n";
+getchar();
 
 }
